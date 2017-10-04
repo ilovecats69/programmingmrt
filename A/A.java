dcb9fcff0625ac6b3434bdd8e753704651ab0c84
mhs <mhs@MHS-Science2-14>
2017-10-04 16:52:32 -0700

    ol.ujk

---
+++ A/A.java
@@ -0,0 +1,14 @@
+public class A {
+	public static void main(String args []){
+		int a = 1;
+		int b = 2;
+		int c = 3;
+		int d = 4;
+		int spicy = 72;
+		double e = 5.5;
+		char izard = 'f';
+		boolean g = true;
+		String cheese = "You are here";
+	}
+
+}
