07ac7d0977c75268b7b519b98bc509fabf9a5877
mhs <mhs@MHS-Science2-14>
2017-10-11 16:57:32 -0700

    C

---
+++ A/Statements.java
@@ -0,0 +1,19 @@
+
+public class Statements {
+	public static void main(String args []){
+		int a = 22;
+		int b = 9;
+		int c = 16;
+		if(a>b && a>c){
+			System.out.println(a);}
+		if(b>a && b>c){
+			System.out.println(b);}
+		if(c>a && c>b){
+			System.out.println(c);}	
+		int f = 9; 
+		while(f<12){
+			System.out.println("go programming");
+			f++;
+		}
+	}
+}
